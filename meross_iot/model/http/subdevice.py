c082ac447778bcfe9436c55923a93a67e1e05003
Paweł Chromiński <zborek8@gmail.com>
2026-01-20 19:43:21 +0100

    fix

--- meross_iot/model/http/subdevice.py
+++ meross_iot/model/http/subdevice.py
@@ -9,18 +9,18 @@ _LOGGER = logging.getLogger(__name__)
 class HttpSubdeviceInfo(BaseDictPayload):
     def __init__(self,
                  sub_device_id: str,
-                 true_id: str,
+#                 true_id: str,
                  sub_device_type: str,
-                 sub_device_vendor: str,
+#                 sub_device_vendor: str,
                  sub_device_name: str,
                  sub_device_icon_id: str,
                  *args, **kwargs):
 
         super().__init__(*args, **kwargs)
         self.sub_device_id = sub_device_id
-        self.true_id = true_id
+#        self.true_id = true_id
         self.sub_device_type = sub_device_type
-        self.sub_device_vendor = sub_device_vendor
+#        self.sub_device_vendor = sub_device_vendor
         self.sub_device_name = sub_device_name
         self.sub_device_icon_id = sub_device_icon_id
 
@@ -28,5 +28,6 @@ class HttpSubdeviceInfo(BaseDictPayload):
         return json.dumps(self.__dict__)
 
     def __str__(self):
-        basic_info = f"{self.sub_device_name} ({self.sub_device_type}, ID {self.sub_device_id}, TRUE-ID {self.true_id})"
+#        basic_info = f"{self.sub_device_name} ({self.sub_device_type}, ID {self.sub_device_id}, TRUE-ID {self.true_id})"
+        basic_info = f"{self.sub_device_name} ({self.sub_device_type}, ID {self.sub_device_id})"
         return basic_info
